5122ad1ae131cb7ebb108d9a6962b99caf79cd46
ishaumar123 <isha.umarfarooq@devsinc.com>
2023-03-07 23:22:40 +0500

    added the auth header

--- src/lib/apollo/client.tsx
+++ src/lib/apollo/client.tsx
@@ -11,24 +11,24 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   }
 })
 
-const link = ApolloLink.from([new HttpLink({ uri: 'http://localhost:4000/graphql' }), errorLink])
+const httpLink = ApolloLink.from([new HttpLink({ uri: 'http://localhost:4000/graphql' }), errorLink])
 const authLink = new ApolloLink((operation, forward) => {
   // Retrieve the authorization token from local storage.
-  const token = Cookies.get('access_token');
+  const token = Cookies.get('access_token')
 
   // Use the setContext method to set the HTTP headers.
   operation.setContext({
     headers: {
       authorization: token ? `Bearer ${token}` : ''
     }
-  });
+  })
 
   // Call the next link in the middleware chain.
-  return forward(operation);
-});
+  return forward(operation)
+})
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: link
+  link: authLink.concat(httpLink)
 })
 
 export default client
