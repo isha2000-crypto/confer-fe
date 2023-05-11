857df56b4a187ea63bd1219f3ec15a46a8ceb7d2
danish <muhammad.danish@devsinc.com>
2023-05-11 22:31:31 +0500

    removed unused code

--- src/components/molecules/Dialog/DialogTenant/DialogTenantCreate.tsx
+++ src/components/molecules/Dialog/DialogTenant/DialogTenantCreate.tsx
@@ -2,17 +2,6 @@ import CreateTenant from '@components/organisms/Forms/CreateTenant'
 import { Dialog, DialogTitle, Typography, DialogContent } from '@mui/material'
 import React from 'react'
 
-// import TableRoleEdit from '@components/molecules/Table/TableRoleEdit'
-// import Alert from '@mui/material/Alert'
-
-// // ** Redux Imports
-// import { useDispatch } from 'react-redux'
-// import { AppDispatch, RootState } from 'src/store'
-// import { addNewRole, updateRole } from 'src/store/roles/rolesActions'
-// import { useSelector } from 'react-redux'
-// import { clearErrors } from 'src/store/roles/rolesSlice'
-// import { FORM_ACTIONS } from '@custom-types/enum'
-
 interface Props {
   handleClose: any
   open: boolean
@@ -20,8 +9,6 @@ interface Props {
 }
 
 function DialogTenantCreate({ handleClose, open, dialogTitle }: Props) {
-  // const dispatch = useDispatch<AppDispatch>()
-
   return (
     <Dialog fullWidth maxWidth='md' scroll='body' onClose={handleClose} open={open}>
       <DialogTitle sx={{ textAlign: 'center' }}>
