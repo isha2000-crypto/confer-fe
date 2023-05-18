80d5f84050a45015dbfa5ac9e95a7fbbd9c66465
ishaumar123 <isha.umarfarooq@devsinc.com>
2023-05-18 17:02:00 +0500

    resolve the linter issues

--- src/pages/profile/edit/index.tsx
+++ src/pages/profile/edit/index.tsx
@@ -1,25 +1,17 @@
-// import React from 'react'
-// import { useAuth } from 'src/hooks/useAuth'
-
-// const Profile = () => {
-//   const auth = useAuth()
-// }
-// export default Profile
-
 // ** React Imports
-import { ChangeEvent, MouseEvent, useState, SyntheticEvent } from 'react'
+import { ChangeEvent, MouseEvent, useState } from 'react'
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import Grid from '@mui/material/Grid'
-import Link from '@mui/material/Link'
+
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import CardHeader from '@mui/material/CardHeader'
 import InputLabel from '@mui/material/InputLabel'
 import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
+
 import CardContent from '@mui/material/CardContent'
 import FormControl from '@mui/material/FormControl'
 import OutlinedInput from '@mui/material/OutlinedInput'
