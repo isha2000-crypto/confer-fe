43e7c8d2b068b9577af875c3a1bc3d8cf0ac6cfc
danish <muhammad.danish@devsinc.com>
2023-06-01 19:00:49 +0500

    fixedbuild issues

--- src/components/organisms/UsersList/index.tsx
+++ src/components/organisms/UsersList/index.tsx
@@ -31,7 +31,7 @@ function UsersList() {
           </Typography>
         }
       />
-      <TableUsersList users={usersStore.users} anchor={false} header={false} />
+      <TableUsersList users={usersStore.users} />
     </>
   )
 }
