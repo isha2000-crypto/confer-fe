b4cb96d0fb9d05f9b176f6da220f511b476ba8f5
danish <muhammad.danish@devsinc.com>
2023-06-07 18:04:43 +0500

    updated filter type for created_at field

--- src/components/molecules/Table/TableAllAssessment/columns.tsx
+++ src/components/molecules/Table/TableAllAssessment/columns.tsx
@@ -5,6 +5,9 @@ import { formatDate } from 'src/@core/utils/format'
 import { displayTime } from 'src/utils/timeFuncs'
 import RenderCustomAvatar from '../RenderCustomAvatar'
 import { useMemo } from 'react'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 interface CellType {
   row: any
@@ -94,7 +97,24 @@ const AllAssessmentTableColumns = () => {
         accessorFn: (row: { createdAt: string | Date }) => {
           return formatDate(row.createdAt)
         },
-        header: 'Created At'
+        header: 'Created At',
+        Filter: ({ column }) => (
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              onChange={newValue => {
+                column.setFilterValue(newValue)
+              }}
+              slotProps={{
+                textField: {
+                  helperText: 'Filter Mode: Less Than',
+                  sx: { minWidth: '120px' },
+                  variant: 'standard'
+                }
+              }}
+              value={column.getFilterValue()}
+            />
+          </LocalizationProvider>
+        )
       }
     ],
     []
